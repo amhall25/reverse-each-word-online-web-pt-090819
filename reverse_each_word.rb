c38ed03fc53997532285cb4832b33433e6ffbe70
alycia hall <amhall25@gmail.com>
2019-09-16 16:52:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   array=sentence.split(" ")
   reverse=[]
-  array.each do |word|
+  array.collect do |word|
     reverse<< word.reverse
-    puts reverse
+    
   end
   return reverse.join(" ")
 end
